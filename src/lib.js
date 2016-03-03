a409601b0eb50063835754788ff0885c77668a4e
Bret K. Ikehara <Bret.K.Ikehara@gmail.com>
2016-03-03 02:04:33 -0800

    simply the cli interface

--- src/lib.js
+++ src/lib.js
@@ -1,4 +1,5 @@
 var argv = require('yargs').argv,
+    fs = require('fs'),
     request = require('request'),
     qs = require('querystring'),
     glob = require('glob');
@@ -7,7 +8,39 @@ function getHost () {
   return argv.host || 'http://localhost:5000'
 }
 
-function retrieveToken (tokenCfg) {
+function upload (url, secret, token, body) {
+  return new Promise(function (resolve, reject) {
+    request.post(url + '?' + qs.stringify({
+      'secret': secret,
+      'token': token,
+    }), {
+      body: JSON.stringify(body)
+    }, function (error, response, body) {
+      if (error) {
+        reject({
+          'error': error,
+          'response': response,
+        });
+      } else {
+        resolve({
+          'response': response,
+          'body': body,
+        });
+      }
+    });
+  });
+}
+
+exports.version = function () {
+  fs.readFile(__dirname + '/../package.json', 'utf8', function (err, data) {
+    if (err) {
+      console.error(colors.red(err));
+    }
+    console.log('alm-cli version:', JSON.parse(data).version);
+  });
+};
+
+exports.retrieveToken = function (tokenCfg) {
   return new Promise(function (resolve, reject) {
     request(getHost() + '/auth/token?' + qs.stringify(tokenCfg), function (error, response, body) {
       if (error) {
@@ -26,7 +59,7 @@ function retrieveToken (tokenCfg) {
 };
 
 // options is optional
-function gatherLocalizationBundles (localizationCfg) {
+exports.gatherLocalizationBundles = function (localizationCfg) {
   // TODO FIX VALIDATION
   return new Promise(function (resolve, reject) {
     var languages = {},
@@ -46,6 +79,7 @@ function gatherLocalizationBundles (localizationCfg) {
       resolve({
         'basePath': localizationCfg.basePath,
         'languages': Object.keys(languages),
+        'fileExtension': localizationCfg.fileExtension,
         'bundles': Object.keys(bundles).map(function (bundle) {
           return bundle.substring(0, bundle.indexOf(localizationCfg.fileExtension))
         }),
@@ -54,18 +88,34 @@ function gatherLocalizationBundles (localizationCfg) {
   });
 };
 
-exports.generateConfig = function (localizationCfg, tokenCfg) {
-  return gatherLocalizationBundles(localizationCfg).then(function (localizationCfg) {
-    return retrieveToken(tokenCfg).then(function (resp) {
+exports.readLocalizationBundle = function (path) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(path, {'enc': 'utf8'}, function (err, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+exports.config = function (localizationCfg, tokenCfg) {
+  return exports.gatherLocalizationBundles(localizationCfg).then(function (cfg) {
+    if (!tokenCfg) {
+      return cfg;
+    }
+
+    return exports.retrieveToken(tokenCfg).then(function (resp) {
       if (resp.error) {
         return Promise.reject(new Error('response error'));
       }
-      localizationCfg.token = JSON.parse(resp.body).token;
-      return localizationCfg;
+      cfg.token = JSON.parse(resp.body).token;
+      return cfg;
     }).catch(function (o) {
       console.error(colors.red(o.error.message), o.response);
-      localizationCfg.token = 'request error';
-      return localizationCfg;
+      cfg.token = 'request error';
+      return cfg;
     });
   });
 };
@@ -94,24 +144,22 @@ exports.uploadConfig = function (bucket, secret, localizationCfg) {
 };
 
 exports.uploadLocales = function (bucket, secret, localizationCfg) {
-  return new Promise(function (resolve, reject) {
-    request.post(getHost() + '/upload/' + bucket + '?' + qs.stringify({
-      'secret': secret,
-      'token': localizationCfg.token,
-    }), {
-      body: JSON.stringify(localizationCfg)
-    }, function (error, response, body) {
-      if (error) {
-        reject({
-          'error': error,
-          'response': response,
-        });
-      } else {
-        resolve({
-          'response': response,
-          'body': body,
-        });
-      }
-    });
+  var uploads = [];
+  localizationCfg.languages.forEach(function (lang) {
+    uploads.concat(localizationCfg.bundles.map(function (bundle) {
+      return exports.readLocalizationBundle([lang, bundle].join('/')).then(function (body) {
+        return upload([getHost(), 'upload', bucket, lang, bundle].join('/'), secret, token, body);
+      });
+    }));
+  });
+  return uploads;
+};
+
+exports.upload = function (localizationCfg, tokenCfg, bucket) {
+  return exports.config(localizationCfg, tokenCfg).then(function (localizationCfg) {
+    return Promise.all([
+      exports.uploadConfig(bucket, tokenCfg.secret, localizationCfg),
+      exports.uploadLocales(bucket, tokenCfg.secret, localizationCfg),
+    ]);
   });
 };
